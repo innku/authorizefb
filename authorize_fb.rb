9b54d0e69793d61d488a83f6e85c8358b794be7a
Adrian Cuadros <adrian@innku.com>
2011-08-20 19:43:05 -0500

    tries stuff

--- authorize_fb.rb
+++ authorize_fb.rb
@@ -7,8 +7,8 @@ end
 
 get '/auth/facebook/callback' do
   @user_token = request.env['omniauth.auth']["credentials"]["token"]
-  #user = FGraph::User.me(@user_token)
-  # @accounts = user.accounts
-  # @accounts = user.accounts.collect {|account| {:name => account.name, :access_token => account.access_token} }
+  user = FbGraph::User.me(@user_token)
+  @accounts = user.accounts
+  @accounts = user.accounts.collect {|account| {:name => account.name, :access_token => account.access_token} }
   erb :index
 end
