06b9c5ed560d29b499639091c21fce673c31d04e
Adrian Cuadros <adrian@innku.com>
2011-11-19 18:17:23 -0600

    Adds some comments to explain the app

--- authorize_fb.rb
+++ authorize_fb.rb
@@ -5,6 +5,8 @@ get '/' do
   erb :index
 end
 
+## After authorization, shows you access tokens for all your applications and pages
+## Notice it doesnt save anywhere
 get '/auth/facebook/callback' do
   @user_token = request.env['omniauth.auth']["credentials"]["token"]
   user = FbGraph::User.me(@user_token)
--- config.ru
+++ config.ru
@@ -5,6 +5,8 @@ require './authorize_fb'
 
 use Rack::Session::Cookie
 
+## Uses Omniauth to gain the right permissions to access your pages
+##
 use OmniAuth::Builder do
 	provider :facebook, "your-facebook-app-id", #FB APP ID
 	                    "your-facebook-app-secret", #FB APP SECRET
